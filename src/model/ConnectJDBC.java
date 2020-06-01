277ad481b6798a65836ab84178049b7d0ec8adad
Nina <nina.kaveh1998@gmail.com>
2020-06-01 20:40:08 +0200

    Set default credentials for database connection

--- src/model/ConnectJDBC.java
+++ src/model/ConnectJDBC.java
@@ -11,7 +11,7 @@ public class ConnectJDBC {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             String userName = "root";
-            String password = "root";
+            String password = "";
             String url = "jdbc:mysql://localhost:3306/groot?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
             connection = DriverManager.getConnection(url, userName, password);
         } catch (Exception e) {
